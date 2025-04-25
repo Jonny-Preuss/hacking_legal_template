aaec718a42b69c13e624eae183c804e448c8efc5
jonny_preuss <jspreuss@horvath-partners.com>
2025-04-25 20:06:19 +0200

    updates and test notebook

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
